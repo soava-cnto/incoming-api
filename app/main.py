c69902cb78cc48fbee9e82e66cfdc9032c6694ce
Soava Cnto <soava.cnto@gmail.com>
2025-10-20 12:33:50 +0300

    update main, create template html for the root page

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import ingest, export, scheduler as scheduler_router  # 👈 on renomme ici
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.jobs.sftp_ingest_job import auto_ingest_yesterday
 import logging
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 logging.basicConfig(
     level=logging.INFO,
@@ -24,16 +25,8 @@ app.include_router(ingest.router, prefix="/ingest", tags=["Ingestion"])
 app.include_router(export.router, prefix="/export", tags=["Export"])
 app.include_router(scheduler_router.router, prefix="/scheduler", tags=["Scheduler"])  # 👈 corrigé
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/", response_class=HTMLResponse)
-def root():
-    html_content = """
-    <html>
-        <head>
-            <title>Ma Page</title>
-        </head>
-        <body>
-            <h1>Bienvenue dans Incoming API 🚀</h1>
-        </body>
-    </html>
-    """
-    return html_content #{"message": "Bienvenue dans Incoming API 🚀"}
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
